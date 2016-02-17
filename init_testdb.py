99a801dcd7792a4717eaf9eb1adfa9d9133f13a7
Kevin Zeng <kzeng10@gmail.com>
2016-02-17 15:02:40 -0500

    need to append to list

--- init_testdb.py
+++ init_testdb.py
@@ -36,16 +36,18 @@ for b in db.query(Business).order_by(Business.id)[0:5]:
     # print b
     # print b.__dict__
     businesslist.append(b.__dict__)
-    bcategorieslist = [cat for cat in db.query(business_category_table).filter(business_category_table.c.business_id == b.id)[:]]
-    docasoclist = [asoc for asoc in db.query(document_associations).filter(document_associations.c.assoc_id == b.id)[:]]
-    doclist = [doc for doc in db.query(documents).filter(documents.c.assoc_id == b.id)[:]]
-    locationslist = [loc for loc in db.query(locations).filter(locations.c.street_address == b.location_address)[:]]
-    reviewslist = [review for review in db.query(yelp_reviews).filter(yelp_reviews.c.business_id == b.id)[:]]
+    bcategorieslist += [cat for cat in db.query(business_category_table).filter(business_category_table.c.business_id == b.id)[:]]
+    docasoclist += [asoc for asoc in db.query(document_associations).filter(document_associations.c.assoc_id == b.id)[:]]
+    doclist += [doc for doc in db.query(documents).filter(documents.c.assoc_id == b.id)[:]]
+    locationslist += [loc for loc in db.query(locations).filter(locations.c.street_address == b.location_address)[:]]
+    reviewslist += [review for review in db.query(yelp_reviews).filter(yelp_reviews.c.business_id == b.id)[:]]
     # ycategorieslist = [cat for cat in db.query(categories, business_category_table).filter(business_category_table.c.business_id == b.id).filter(business_category_table.c.category_alias == categories.alias)[:]]
 
 # print businesslist
 # print bcategorieslist
 test_db.execute(businesses.insert(), businesslist)
+print bcategorieslist
+print docasoclist
 test_db.execute(business_category_table.insert(), bcategorieslist)
 test_db.execute(document_associations.insert(), docasoclist)
 test_db.execute(documents.insert(), doclist)
